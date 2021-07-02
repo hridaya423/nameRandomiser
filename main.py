e787a02f51976cf71a74ffb0995a9ad4aeeaa1ef
Hridaya Agrawal <hridayahoney@gmail.com>
2021-07-02 14:28:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from random import choice
+from flask import Flask, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__, template_folder='')
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    students = ['Hridaya', 'Sadhana', 'Rajaharshini', 'Priyadarshan', 'Priyam', 'Mohit', 'Sunidhi', 'Seherish',
+                'Dikshith', 'Aradhana', 'Aarav', 'Siyona', 'Apparnachar', 'Nikash', 'Darshan', 'Ashwini', 'Srinidhi',
+                'Aparna', 'Maya', 'Gnanedra', 'Pramita']
+    return render_template('index.html', name=choice(students))
+
+
+app.run(debug=True)
